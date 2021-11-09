028b6e6fc8faa8bb1aeb8450095bba9ba355cf75
superhan-dev <superhan.dev@gmail.com>
2021-11-10 08:38:10 +0900

    비즈니스 로직에서 에러처리를 글로벌하게 처리하도록 수정

--- src/main/java/com/superhan/springbootblog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/superhan/springbootblog/handler/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.superhan.springbootblog.handler;
 
+import com.superhan.springbootblog.dto.ResponseDto;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value=IllegalArgumentException.class)
-    public String handleArgumentException(IllegalArgumentException e){
-        return "<h1>"+e.getMessage()+"</h1>";
+    @ExceptionHandler(value = Exception.class)
+    public ResponseDto<String> handleArgumentException(Exception e) {
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                e.getMessage());
     }
 }
